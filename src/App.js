842b49a66b4b2fe15fbdc2cee30b233b7be15a1f
tarik.erdogan <salih.erdogan@param.com.tr>
2021-12-08 09:49:01 +0300

    Kurulum

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import PivotGrid, { FieldChooser } from "devextreme-react/pivot-grid";
+import PivotGridDataSource from "devextreme/ui/pivot_grid/data_source";
+
+import { sales } from "./data.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="long-title">
+        <h3>Sales Amount by Region</h3>
+      </div>
+      <PivotGrid
+        id="sales"
+        dataSource={dataSource}
+        allowSortingBySummary={true}
+        allowSorting={true}
+        allowFiltering={true}
+        allowExpandAll={true}
+        height={440}
+        showBorders={true}
+      >
+        <FieldChooser enabled={false} />
+      </PivotGrid>
+    </React.Fragment>
   );
 }
+const dataSource = new PivotGridDataSource({
+  fields: [
+    {
+      caption: "Region",
+      width: 120,
+      dataField: "region",
+      area: "row",
+    },
+    {
+      caption: "City",
+      dataField: "city",
+      width: 150,
+      area: "row",
+      selector(data) {
+        return `${data.city} (${data.country})`;
+      },
+    },
+    {
+      dataField: "date",
+      dataType: "date",
+      area: "column",
+    },
+    {
+      caption: "Sales",
+      dataField: "amount",
+      dataType: "number",
+      summaryType: "sum",
+      format: "currency",
+      area: "data",
+    },
+  ],
+  store: sales,
+});
 
 export default App;
